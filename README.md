e70652d851030aaa64bfc44f7f5d0a0f1d8cb566
Scott Travis <scott.w.travis@gmail.com>
2019-09-26 16:41:03 +0000

    Working.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # GDriver
 
+A PHP library for converting GDrive links.
+
+## Install
+
+Normal install via Composer.
+
+## Usage
+
+When you copy a link to a remote GDrive file, the link provided will be to a GDrive page.  This package converts the URL provided into a link that takes you to the actual file instead.
+
+```php
+
+// link provided
+$provided = 'https://drive.google.com/open?id=YOURFILEID&authuser=YOUREMAIL&usp=drive_fs';
+
+// link converted
+$converted = GDriver::convert($provided);
+
+// https://drive.google.com/uc?export=download&id=YOURFILEID
+echo $converted;
+```
--- src/GDriver.php
+++ src/GDriver.php
@@ -4,8 +4,32 @@ namespace Travis;
 
 class GDriver
 {
-    public static function run($string)
+    /**
+     * Return a converted link to a direct file download from GDrive.
+     *
+     * @param   string  $url
+     * @return  string
+     */
+    public static function convert($url)
     {
+        // if not google...
+        if (!stripos($url, 'drive.google.com'))
+        {
+            // throw error
+            throw new \Exception('Invalid GDrive link provided.');
+        }
 
+        // parse url
+        $parts = parse_url($url);
+        parse_str($parts['query'], $args);
+
+        // get id
+        $id = isset($args['id']) ? $args['id'] : null;
+
+        // catch error...
+        if (!$id) throw new \Exception('Invalid GDrive link provided.');
+
+        // reformat
+        return 'https://drive.google.com/uc?export=download&id='.$id;
     }
 }
